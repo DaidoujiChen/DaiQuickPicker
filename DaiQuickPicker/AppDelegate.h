4754203963e365727acbef92b096f743b2a5e045
DaidoujiChen <daidoujichen@gmail.com>
2014-06-18 10:02:15 +0800

    Initial Commit

---
+++ DaiQuickPicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DaiQuickPicker
+//
+//  Created by 啟倫 陳 on 2014/6/18.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DaiQuickPickerTests/DaiQuickPickerTests.m
@@ -0,0 +1,34 @@
+//
+//  DaiQuickPickerTests.m
+//  DaiQuickPickerTests
+//
+//  Created by 啟倫 陳 on 2014/6/18.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DaiQuickPickerTests : XCTestCase
+
+@end
+
+@implementation DaiQuickPickerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
